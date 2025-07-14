61603a384245ab4a0ee3a46c29dd50982ded68c6
Shashank Mishra <shashankmishra1816@gmail.com>
2025-07-14 11:07:23 +0530

    frontend

--- main.js
+++ main.js
@@ -40,4 +40,44 @@ ScrollReveal().reveal(".header__content img", {
   delay: 1500,
 });
 
+ScrollReveal().reveal(".about__content .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".about__signature", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".tour__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+ScrollReveal().reveal(".destination__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+});
+
+ScrollReveal().reveal(".blog__card", {
+  ...scrollRevealOption,
+  interval: 500,
+});
+
+const banner = document.querySelector(".banner__wrapper");
+const bannerImages = Array.from(banner.children);
+
+bannerImages.forEach((item) => {
+  const duplicateNode = item.cloneNode(true);
+  duplicateNode.setAttribute("aria-hidden", true);
+  banner.appendChild(duplicateNode);
+});
+
 
